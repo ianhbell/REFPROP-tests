b11011d137467ef559e19d91c58f88a7ed003d2b
Ian Bell <ian.h.bell@gmail.com>
2017-12-03 20:49:16 -0700

    Fix the tests - what is up with stdout?

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <cstdlib>
 
 #define CATCH_CONFIG_MAIN
@@ -12,12 +11,16 @@ class REFPROPDLLFixture
 {
 public:
     REFPROPDLLFixture(){
-        const char* RPPREFIX = std::getenv("RPPREFIX");
-        std::cout << RPPREFIX << std::endl;
-        std::string shared_library_filename = "hihi";
+        char* RPPREFIX = std::getenv("RPPREFIX");
+        REQUIRE(strlen(RPPREFIX) != 0);
+        std::string shared_library_filename = "";
         std::string err = "RRRRRRRRRRRRRRRRRRRRRRRRRRRRRR";
         bool did_load = load_REFPROP(err, RPPREFIX, shared_library_filename);
         REQUIRE(did_load);
+        
+        // Set the path in REFPROP
+        REQUIRE(SETUPdll != nullptr);
+        SETPATHdll(RPPREFIX, strlen(RPPREFIX));
     }
     ~REFPROPDLLFixture(){
         std::string err;
@@ -30,31 +33,18 @@ public:
 class NBP : public REFPROPDLLFixture
 {
 public:
-    NBP() : REFPROPDLLFixture() { 
-        std::cout << "NBP init\n"; 
-    };
     void payload(){
-        std::cerr << "payyy\n";
-        
-        const char* RPPREFIX = std::getenv("RPPREFIX");
-        REQUIRE(SETUPdll != nullptr);
-        SETPATHdll(RPREFIX, strlen(RPPREFIX));
-
         long ierr = 1, nc = 1;
         char herr[255], hfld[] = "WATER.FLD", hhmx[] = "HMX.BNC", href[] = "DEF";
         SETUPdll(nc,hfld,hhmx,href,ierr,herr,10000,255,3,255);
         CHECK(ierr==0);
         
-        /*if (ierr > 0) printf("This ierr: %ld herr: %s\n", ierr, herr);
-        {
-            long ierr = 0;
-            char herr[255];
-            double z[] = {1.0}, x[] = {1.0}, y[] = {1.0}, T= 300, p = 101.325, d = -1, dl = -1, dv = -1, h = -1, s = -1, u = -1, cp = -1, cv = -1, q = -1, w = -1;
-            TPFLSHdll(T, p, z, d, dl, dv, x, y, h,s,u,cp,cv,w,q,ierr,herr,255);
-            if (ierr > 0) printf("This ierr: %ld herr: %s\n", ierr, herr);
-            printf("This d: %g mol/L\n", d);
-        }
-        */
+        double z[] = {1.0}, x[] = {1.0}, y[] = {1.0}, T= 300, p = 101.325, d = -1, dl = -1, dv = -1, h = -1, s = -1, u = -1, cp = -1, cv = -1, q = -1, w = -1;
+        TPFLSHdll(T, p, z, d, dl, dv, x, y, h,s,u,cp,cv,w,q,ierr,herr,255);
+        CHECK(ierr == 0);
+        CAPTURE(herr);
+        
+        //std::cout << "d: "+std::to_string(d)+" mol/L\n";
     }
 };
 
