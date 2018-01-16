aa1f9993a7c43cb48d1b589d3f27d8bdaf82bf1b
Ian Bell <ian.bell@nist.gov>
2018-01-16 12:07:46 -0700

    Add forward and reverse tests of NBP for all fluids

---
+++ include/REFPROPtests/utils.h
@@ -0,0 +1,27 @@
+#ifndef REFPROP_TESTS_UTILS
+#define REFPROP_TESTS_UTILS
+
+#include <boost/filesystem.hpp>
+#include <boost/foreach.hpp> 
+
+std::vector<std::string> get_pure_fluids_list() {
+    char* RPPREFIX = std::getenv("RPPREFIX");
+    REQUIRE(strlen(RPPREFIX) != 0);
+    namespace fs = boost::filesystem;
+    std::vector<std::string> fluids;
+
+    // See https://stackoverflow.com/a/8725664/1360263
+    fs::path targetDir(fs::path(RPPREFIX) /  fs::path("FLUIDS"));
+    fs::directory_iterator it(targetDir), eod;
+    BOOST_FOREACH(fs::path const &p, std::make_pair(it, eod))
+    {
+        if (fs::is_regular_file(p) && p.extension() == fs::path(".FLD"))
+        {
+            std::string s = fs::canonical(p).filename().replace_extension("").string();
+            fluids.push_back(s);
+        }
+    }
+    return fluids;
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include "catch.hpp"
 
 #include "REFPROPtests/baseclasses.h"
+#include "REFPROPtests/utils.h"
+
 
 class NBP : public REFPROPDLLFixture
 {
@@ -49,6 +51,47 @@ public:
 };
 
 TEST_CASE_METHOD(mixNBP, "Check TP flash of multicomponent mixture", "[flash]")
+{
+    payload();
+};
+
+class pureNBP : public REFPROPDLLFixture
+{
+public:
+    void payload() {
+        for (auto &fld : get_pure_fluids_list()) {
+            int ierr = 0;
+            char cfld[10000]; 
+            strcpy(cfld, fld.c_str());
+            SETFLUIDSdll(cfld, ierr, 255);
+            REQUIRE(ierr == 0); 
+            
+            double wmm, ttrp, tnbpt, tc, pc, Dc, Zc, acf, dip, Rgas, z[] = {1.0};
+            int icomp = 1, kq = 1;
+            INFOdll(icomp, wmm, ttrp, tnbpt, tc, pc, Dc, Zc, acf, dip, Rgas);
+
+            double T, p=101.325, x[20], D, Dl, Dv, xliq[20], xvap[20], q=0, u,h,s,cv,cp,w;
+            ierr=0; char herr[255]="";
+            // Calculate p_nbp given T_nbp
+            PQFLSHdll(p, q, z, kq, T, D, Dl, Dv, xliq, xvap, u,h,s,cv,cp,w,ierr, herr, 255);
+            CAPTURE(fld);
+            CHECK(ierr == 0);
+            CHECK(T == Approx(tnbpt).margin(0.1));
+            CAPTURE(herr);
+
+            // Now do the inverse calculation T_nbp->p_nbp
+            T = tnbpt;
+            TQFLSHdll(T, q, z, kq, p, D, Dl, Dv, xliq, xvap, u, h, s, cv, cp, w, ierr, herr, 255);
+            CAPTURE(fld);
+            CHECK(ierr == 0);
+            CHECK(p == Approx(101.325).margin(0.1));
+            CAPTURE(herr);
+
+        }
+    }
+};
+
+TEST_CASE_METHOD(pureNBP, "Check NBP for all pure fluids (when possible)", "[flash]")
 {
     payload();
 };
