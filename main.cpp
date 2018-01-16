043fabbdfb35b4b9112e64df20a6db5d9ba80f90
Ian Bell <ian.bell@nist.gov>
2018-01-16 10:00:51 -0700

    Add TP flash failure

--- main.cpp
+++ main.cpp
@@ -28,3 +28,27 @@ TEST_CASE_METHOD(NBP, "Check NBP of water", "[nbp]")
 {
     payload();
 };
+
+class mixNBP : public REFPROPDLLFixture
+{
+public:
+    void payload() {
+        char hfld[10000] = "NITROGEN|WATER|CO2|H2S|METHANE|ETHANE|PROPANE|ISOBUTAN|BUTANE|IPENTANE|PENTANE";
+        double z[] = {1.2000036000108E-03,7.000021000063E-06,.828792486377459,2.000006000018E-04,.160400481201444,7.6000228000684E-03,1.4000042000126E-03,1.000003000009E-04,2.000006000018E-04,0,1.000003000009E-04};
+        int ierr = 0, nc = 11;
+        char herr[255], hhmx[] = "HMX.BNC", href[] = "DEF";
+        SETUPdll(nc, hfld, hhmx, href, ierr, herr, 10000, 255, 3, 255);
+        REQUIRE(ierr == 0);
+
+        ierr = 0;
+        double x[30] = { 1.0 }, y[20] = { 1.0 }, T = 313.15, p = 400, d = -1, dl = -1, dv = -1, h = -1, s = -1, u = -1, cp = -1, cv = -1, q = 0, w = -1;
+        TPFLSHdll(T, p, z, d, dl, dv, x, y, q, u, h, s, cp, cv, w, ierr, herr, 255);
+        CAPTURE(herr); 
+        REQUIRE(ierr == 0);
+    }
+};
+
+TEST_CASE_METHOD(mixNBP, "Check TP flash of multicomponent mixture", "[flash]")
+{
+    payload();
+};
