5ecd6adae4c0f8667adbd7c3277edb87ed4e93ea
Ian Bell <ian.bell@nist.gov>
2018-01-16 12:22:34 -0700

    Test Ttriple, capture errors better

--- main.cpp
+++ main.cpp
@@ -19,8 +19,9 @@ public:
         int kq = 1;
         double z[] = {1.0}, x[] = {1.0}, y[] = {1.0}, T= 300, p = 101.325, d = -1, dl = -1, dv = -1, h = -1, s = -1, u = -1, cp = -1, cv = -1, q = 0, w = -1;
         PQFLSHdll(p, q, z, kq, T, d, dl, dv, x, y, u,h,s,cp,cv,w,ierr,herr,255);
+        CAPTURE(herr); 
         CHECK(ierr == 0);
-        CAPTURE(herr);
+        
 
         CHECK(T == Approx(373.15).margin(0.1));
     }
@@ -70,24 +71,24 @@ public:
             int icomp = 1, kq = 1;
             INFOdll(icomp, wmm, ttrp, tnbpt, tc, pc, Dc, Zc, acf, dip, Rgas);
 
-            double T, p=101.325, x[20], D, Dl, Dv, xliq[20], xvap[20], q=0, u,h,s,cv,cp,w;
+            double T, p=101.325, D, Dl, Dv, xliq[20], xvap[20], q=0, u,h,s,cv,cp,w;
             ierr=0; char herr[255]="";
 
             // Calculate T_nbp given p_nbp (101.325 kPa)
             PQFLSHdll(p, q, z, kq, T, D, Dl, Dv, xliq, xvap, u,h,s,cv,cp,w,ierr, herr, 255);
             CAPTURE(fld);
+            CAPTURE(herr); 
             CHECK(ierr == 0);
             CHECK(T == Approx(tnbpt).margin(0.1));
-            CAPTURE(herr);
 
             // Now do the inverse calculation T_nbp->p_nbp
             T = tnbpt;
             TQFLSHdll(T, q, z, kq, p, D, Dl, Dv, xliq, xvap, u, h, s, cv, cp, w, ierr, herr, 255);
             CAPTURE(fld);
+            CAPTURE(herr); 
             CHECK(ierr == 0);
             CHECK(p == Approx(101.325).margin(0.1));
-            CAPTURE(herr);
-
+            
         }
     }
 };
@@ -96,3 +97,37 @@ TEST_CASE_METHOD(pureNBP, "Check NBP for all pure fluids (when possible)", "[fla
 {
     payload();
 };
+
+class pureTrip : public REFPROPDLLFixture
+{
+public:
+    void payload() {
+        for (auto &fld : get_pure_fluids_list()) {
+            int ierr = 0; char cfld[10000];
+            strcpy(cfld, fld.c_str());
+            SETFLUIDSdll(cfld, ierr, 255);
+            REQUIRE(ierr == 0);
+
+            double wmm, ttrp, tnbpt, tc, pc, Dc, Zc, acf, dip, Rgas, z[] = { 1.0 };
+            int icomp = 1, kq = 1;
+            INFOdll(icomp, wmm, ttrp, tnbpt, tc, pc, Dc, Zc, acf, dip, Rgas);
+
+            double p = 101.325, x[20], D, Dl, Dv, xliq[20], xvap[20], q = 0, u, h, s, cv, cp, w;
+            ierr = 0; char herr[255] = "";
+
+            char htyp[4] = "EOS"; double Tmin, Tmax, Dmax, Pmax;
+            LIMITSdll(htyp, z, Tmin, Tmax, Dmax, Pmax, 3);
+            if (Tmin > ttrp) {
+                // Skip fluids where Tmin < Trip
+                continue;
+            }
+
+            // Now do the calculation at T_trp
+            TQFLSHdll(ttrp, q, z, kq, p, D, Dl, Dv, xliq, xvap, u, h, s, cv, cp, w, ierr, herr, 255);
+            CAPTURE(fld);
+            CAPTURE(herr);
+            CHECK(ierr == 0);
+        }
+    }
+};
+TEST_CASE_METHOD(pureTrip, "Check Ttriple for all pure fluids (when possible)", "[flash]"){ payload(); };
